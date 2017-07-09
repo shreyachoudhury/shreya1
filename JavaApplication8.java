cbab5611dbad70f3b1104453aaba0464ff1ca6b2
shreyachoudhury <shreyachoudhary.14@gmail.com>
2017-07-09 15:58:25 +0530

    Add files via upload

---
+++ JavaApplication8.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class HelloWorld
+{
+    public static void main(String[] args)throws IOException
+    {
+        int a=1,b=2,c=3,d=4,e=5;
+        int s=0;
+        s=a+b+c+d+e;
+           System.out.println(s);
+        
+        }
+    }
