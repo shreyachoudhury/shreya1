479530e1f8defaba67edaedb1c0372a3db7e1875
shreyachoudhury <shreyachoudhary.14@gmail.com>
2017-07-09 15:53:10 +0530

    Add files via upload

---
+++ JavaApplication7.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class HelloWorld
+{
+    public static void main(String[] args)throws IOException
+    {
+        String s="Hello";
+        int i=0;
+        for(i=1;i<=5;i++)
+        {
+           System.out.println(s);
+        }
+        
+        }
+    }
+
