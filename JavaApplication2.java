47e9705ac598c615e236b2d9d1c5fc0b4d43930a
shreyachoudhury <shreyachoudhary.14@gmail.com>
2017-07-09 14:40:27 +0530

    Add files via upload

---
+++ JavaApplication2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class JavaApplication2
+{
+    public static void main(String[] args)throws IOException
+    {
+        int n=8;
+        
+        if((n%2)==0)
+            System.out.println("It is an even number"+n);
+        if((n%2)!=0)
+            System.out.println("It is a odd number"+n);
+    }
+}
