ae39e2a318e78ae85dbaf1c0be4dad4596f7d523
shreyachoudhury <shreyachoudhary.14@gmail.com>
2017-07-09 14:52:30 +0530

    Add files via upload

---
+++ JavaApplication3.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class JavaApplication3
+{
+    public static void main(String[] args)throws IOException
+    {
+        char ch='a';
+        
+        if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
+            System.out.println("It is a vowel"+ch);
+        else
+            System.out.println("it is a constant");
+            
+    }
+}
