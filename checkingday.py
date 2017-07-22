ac60d252cc76f1dc95d8726a2ac12a3cd361adbc
shreyachoudhury <shreyachoudhary.14@gmail.com>
2017-07-22 14:43:16 +0530

    Add files via upload

---
+++ checkingday.py
@@ -0,0 +1,17 @@
+# Hello World program in Python
+n=raw_input("Enter the working day")
+n=n.lower()
+if (n=='monday'):
+    print('Working day')
+if (n=='tuesday'):
+    print('Working day')
+if (n=='wednesday'):
+    print('Working day')
+if (n=='thursday'):
+    print('Working day')
+if (n=='friday'):
+    print('Working day')
+if (n=='saturday'):
+    print('Working day')
+if (n=='sunday'):
+    print(' holiday')
